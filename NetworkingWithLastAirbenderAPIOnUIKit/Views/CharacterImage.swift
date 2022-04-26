f9a8e4c31c0eafbdbdf04c4e92d8f98cb86372d3
Ruslan <ruslan.shtybaev@gmail.com>
2022-04-26 23:48:04 +0300

    add new view extension

---
+++ NetworkingWithLastAirbenderAPIOnUIKit/Views/CharacterImage.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterImage.swift
+//  NetworkingWithLastAirbenderAPIOnUIKit
+//
+//  Created by Руслан Штыбаев on 26.04.2022.
+//
+
+import UIKit
+
+class CharacterImage: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
